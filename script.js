376dbcd2b5b2c70a7cc5b2b07cecbccb3b393fc8
EricTan <etan47@uwo.ca>
2022-11-19 09:48:55 -0500

    js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let digits = document.getElementsByTagName("h1");
 let buttons = document.getElementsByTagName("button");
+buttons[0].addEventListener('click', startStopwatch);
+let test = digits[0];
+function startStopwatch(a){
+    test.innerText = "01";
+}
