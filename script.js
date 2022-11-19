2d02c67f4521375a09273499b850e81f26561c67
Fred <fmohebi@uwo.ca>
2022-11-19 15:10:37 -0500

    FIX COUNTDOWN

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ rest.addEventListener('click',restWatch);
 reset.addEventListener('click', resetTime);
 
 var timer = new easytimer.Timer();
+var timeSecond;
+let restTime;
+let restMode;
+
+
 
 timer.addEventListener('secondsUpdated', () => {
     const obj = timer.getTimeValues();
@@ -29,6 +34,7 @@ function pauseTime(){
 
 function resetTime(){
     timer.stop();
+    restMode = false;
     start.disabled = false;
     second.innerText = "00";
     minute.innerText = "00";
@@ -36,11 +42,31 @@ function resetTime(){
 }
 
 function restWatch(){
-    second.innerText = "00";
-
+    alert("Be sure to go outside and take a break from the screen");
+    timer.stop();
+    restMode = true;
+    timeSecond = (secondsCNT(parseInt(hour.innerText),parseInt(minute.innerText),parseInt(second.innerText)));
+    restTime = Math.floor(timeSecond/3);
+    console.log(timeSecond);
+    console.log(restTime);
 }
 
 function startStopwatch(){
     timer.start();
+    restMode = false; 
     start.disabled = true;
+}
+
+function secondsCNT(hr,min,sec){
+    return hr*3600+min*60+sec;
+}
+
+function displayTime(sec){
+    hour.innerText = Math.floor(sec / 3600);
+    minute.innerText = math.floor((sec % 3600) / 60);
+    second.innerText = Math.floor((sec % 3600) % 60);
+}
+
+function endTime(){
+    alert("timer Done");
 }
